01ab743a3479371b649cbe7171c7f9c16b51e187
kmpeil <107445694+kmpeil@users.noreply.github.com>
2022-11-19 17:55:53 -0500

    Add files via upload

---
+++ Survival Analysis/SA_HW3_KP.R
@@ -0,0 +1,82 @@
+# import libraries
+library(survival)
+library(foreign)
+library(ggplot2)
+library(survminer)
+library(rms)
+library(flexsurv)
+library(dplyr)
+library(tidyverse)
+library(ciTools)
+library(here)
+library(visreg)
+library(cmprsk)
+
+# read in data
+df = read.csv("https://raw.githubusercontent.com/sjsimmo2/Survival/master/hurricane.csv")
+
+# subset to motor failure
+df.motor = df %>% filter(reason == 2)
+
+# create 12 hours back from failure time
+#df.motor$back12fromfail = df.motor$hour - 12
+
+# make data long
+df.motor$id = seq(1,112)
+
+df.long = pivot_longer(df.motor, cols=h1:h48, names_to="hvars", values_to="status")
+df.long = df.long %>% 
+  rowwise() %>%
+  mutate(hvars = gsub("h","", hvars))
+  
+# censor observations past failure hour
+df.long$hvars = as.numeric(df.long$hvars)
+df.long.censored = df.long %>% filter(hvars <= hour & !is.na(status))
+df.long.censored$fail = ifelse(df.long.censored$survive == 0, 1, 0)
+
+df.long.censored = df.long.censored %>% 
+  group_by(id) %>%
+  mutate(
+    start  = row_number()-1,
+    stop   = row_number()) %>%
+  arrange(id, stop)
+
+
+# create function to count 12 consecutive hours and reset at 0
+CountRunning <- function(status_list){
+  count = 0
+  count_list = c()
+  
+  for (i in status_list){
+    if (i==0){
+      if (count!=0){
+        count <- 0
+      }
+    }
+    else{
+      count <- count + 1
+    }
+    count_list <- append(count_list, c(count))
+  }
+  return(count_list)
+}
+
+
+# apply function to long data frame
+df.long.count <- df.long.censored %>% 
+  group_by(id) %>%
+  mutate(consec_run = CountRunning(status)) %>%
+  mutate(flag12 = ifelse(consec_run >= 12, 1, 0))
+
+# select variables and reorder
+df.final = df.long.count %>%
+  select(id, backup:survive, fail, hour, reason, hvars:flag12)
+
+# export csv
+write.csv(df.final, "/Users/kelsypeil/Desktop/AA502/Survival Analysis/survhw3.csv")
+
+
+
+
+
+
